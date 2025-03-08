d4061f171a98b9e30d238891fb50324b21a1cc6e
dL-hx <897057246@qq.com>
2025-03-08 13:33:55 +0800

    主页美化

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: "/myBlog/",
-  title: "My Awesome Project",
+  title: "小李科技的博客",
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
